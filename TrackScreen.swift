3d2441bbcc99241e169c546f26045007198b22c1
Alex <amartinez5273@stu.d214.org>
2023-10-26 10:23:01 -0500

    added track Screen

---
+++ TrackScreen.swift
@@ -0,0 +1,20 @@
+//
+//  TrackScreen.swift
+//  Better Sports Times 2023
+//
+//  Created by David Popowski on 10/26/23.
+//
+
+import SwiftUI
+
+struct TrackScreen: View {
+    var body: some View {
+        Text("Track is here")
+    }
+}
+
+struct TrackScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        TrackScreen()
+    }
+}
