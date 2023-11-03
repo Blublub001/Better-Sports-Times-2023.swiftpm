d01b5fc6b6bc004ef9daaa1c7ec44488c9fdf9e5
David-Pops <dpopowski6371@stu.d214.org>
2023-11-03 09:58:17 -0500

    Hudles110m changed to look good

--- TrackEvents/Hurdles110m.swift
+++ TrackEvents/Hurdles110m.swift
@@ -8,12 +8,48 @@
 import Foundation
 import SwiftUI
 
-struct Hurdles110m: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+struct playerAndTime: Hashable{
+    var name: String
+    var time: Double
 }
 
+struct Hurdles110m: View {
+    var runners = [
+        playerAndTime(name: "John Brown", time: 15.54),
+        playerAndTime(name: "Philip Kirk", time: 20.61)
+    ]
+    
+    var body: some View {
+        VStack{
+            Text("Hurdles110m")
+                .font(.title)
+                .foregroundColor(.mint)
+            List{
+                ForEach(runners, id:\.self) { runners in
+                    HStack{
+                            Text("\(runners.name)")
+                                .font(.title3)
+                        Rectangle()
+                            .foregroundColor(.white)
+                        
+                            Text("\(String(format: "%.2f", Double(runners.time )))")
+                                .font(.title3)
+                        
+                    }
+                }
+            }
+            
+            Button(action:{
+                    print("one must imagine a player being added")
+                }) {
+                    Text("add player")
+                        .font(.title)
+                }
+                
+            
+        }
+    }
+}
 struct Hurdles110m_Previews: PreviewProvider {
     static var previews: some View {
         Hurdles110m()
